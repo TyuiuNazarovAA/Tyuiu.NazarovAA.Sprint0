867f2d957e4f2a9002faf197b5cf404a94f1ff18
epiccipe <brokeprotocol4916@gmail.com>
2025-09-07 01:03:01 +0500

    Task4

---
+++ Tyuiu.NazarovAA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.NazarovAA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(5, 5));
+            Console.WriteLine(DataService.Subtraction(6, 1));
+            Console.WriteLine(DataService.Multiplication(5, 5));
+            Console.WriteLine(DataService.Division(25, 5));
+
+        }
+    }
+}
