7b151321abbb53a9ce259e8cad5ecbbc7ab042b0
epiccipe <brokeprotocol4916@gmail.com>
2025-09-06 18:12:56 +0500

    Add project files.

---
+++ Tyuiu.NazarovAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Tyuiu.NazarovAA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int age = 17;
+            string name = "Артём";
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет");
+        }
+    }
+}
---
+++ Tyuiu.NazarovAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,3 @@
+﻿using Tyuiu.NazarovAA.Sprint0.Task2.V0.Lib;
+using System;
+Console.WriteLine(DataService.GetMessage("Артём"));
