83e57781b90090b9c04bdb18e64a48248334c2b6
epiccipe <brokeprotocol4916@gmail.com>
2025-09-07 17:52:24 +0500

    Task5

---
+++ Tyuiu.NazarovAA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NazarovAA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.NazarovAA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"5 + 5 = {DataService.Addition(5, 5)}");
+            Console.WriteLine($"5 - 2 = {DataService.Subtraction(5, 2)}");
+            Console.WriteLine($"4 * 6 = {DataService.Multiplication(4, 6)}");
+            
+            //Console.WriteLine($"15 / 0 = {DataService.Division(15, 0)}"); //ошибка
+            
+            Console.WriteLine($"15 / 5 = {DataService.Division(15, 5)}");
+        }
+    }
+}
